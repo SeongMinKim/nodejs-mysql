89f95a05db51dd27bd181e92df7ffb04296d2b39
SeongMinKim <sksmsdhorm@naver.com>
2017-04-26 10:11:55 +0900

    Add cookie-parser

--- app_cookie.js
+++ app_cookie.js
@@ -1,3 +1,23 @@
-/**
- * Created by sksms on 2017-04-26.
- */
+//쿠키
+var express = require('express');
+var cookieParser = require('cookie-parser')
+var app = express();
+
+app.use(cookieParser());
+
+
+app.get('/count', function(req,res){
+   res.cookie('count',1);
+   res.send('count : ');
+});
+
+
+
+
+
+
+
+
+app.listen(3000, function(){
+    console.log('Connect 3000');
+})
--- app_template.js
+++ app_template.js
@@ -1,3 +1,7 @@
-/**
- * Created by sksms on 2017-04-26.
- */
+//예제가 복잡해지거나 단순화 하려면 템플릿을 만들어 놓음
+var express = require('express');
+var app = express();
+
+app.listen(3000, function(){
+    console.log('Con 3000');
+})
