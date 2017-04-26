baba8f944e92942e0e2b8387ac6570351920ce27
SeongMinKim <sksmsdhorm@naver.com>
2017-04-26 10:18:15 +0900

    Cookie Count+1

--- app_cookie.js
+++ app_cookie.js
@@ -7,8 +7,15 @@ app.use(cookieParser());
 
 
 app.get('/count', function(req,res){
-   res.cookie('count',1);
-   res.send('count : ');
+    if(req.cookies.count){ //쿠키가 있다면
+        var count = parseInt(req.cookies.count)
+        //변수로하고 parseInt:자바스크립트 내장함수로 어떠한 값을 정수로 바꿔주는 역할
+    }else{//없다면
+        var count = 0; //초기화를 해준다.
+    }
+   count = count +1; //카운터 값이 1씩 증가
+   res.cookie('count', count);
+   res.send('count : '+req.cookies.count);
 });
 
 
